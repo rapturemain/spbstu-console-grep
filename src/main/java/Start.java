85738d9a32543c6a0de5a3ce43710ddc257a85c4
rapturemain <rapture.main@gmail.com>
2019-02-28 01:49:38 +0300

    Now can be execute from console: java -jar fileName.jar -flags -words -file
    Renamed
    main.java -> Start.java

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,10 +1,14 @@
-public class main {
+
+public class Start {
+
     public static void main(String[] args) {
         if (args.length < 1) throw new IllegalArgumentException();
         StringBuilder argsG = new StringBuilder();
-        for (int i = 0; i < args.length; i++) {
-            argsG.append(args[i + 1]);
+        for (int i = 0; i < args.length - 1; i++) {
+            argsG.append(args[i]);
+            argsG.append(' ');
         }
+        argsG.append(args[args.length - 1]);
         try {
             Grep grep = new Grep(argsG.toString());
             for (String it : grep.getStrings()) {
